4f181a1f7551971db5e400278b074004638b72c5
Adam Hirzalla <adam.hirzalla@carleton.ca>
2022-01-04 04:52:25 -0500

    solve ex7 + add model validations (store/employee)

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter a store name > "
+name = gets.chomp
+store = Store.create(name: name)
+store.errors.messages.each {|col, message| puts "Error -> #{col} #{message.first}"} unless store.valid?
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :has_apparel
 
+  def has_apparel
+    errors.add(:mens_apparel, "or womens apparel needs to be true") unless mens_apparel || womens_apparel 
+  end
 end
